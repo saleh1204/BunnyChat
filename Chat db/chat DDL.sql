b2759a779d91b10ca33ebb05275aad5dd8afc333
Saleh AlSaleh <saleh.lehigh@gmail.com>
2016-09-07 14:12:30 +0300

    Added most updated DDL statements Forgot to include them in the last comment The DDL statement contains creating the Databases along with the tables

--- Chat db/chat DDL.sql
+++ Chat db/chat DDL.sql
@@ -1,96 +1,144 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 07, 2016 at 02:09 PM
+-- Server version: 5.7.13-0ubuntu0.16.04.2
+-- PHP Version: 7.0.8-0ubuntu0.16.04.2
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema chat
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema chat
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `chat` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `chat` ;
-
--- -----------------------------------------------------
--- Table `chat`.`login`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `chat`.`login` ;
-
-CREATE TABLE IF NOT EXISTS `chat`.`login` (
-  `username` VARCHAR(25) NOT NULL,
-  `password` VARCHAR(30) NULL,
-  `admin` TINYINT(1) NULL DEFAULT 0,
-  `Email` VARCHAR(100) NOT NULL,
-  `Gender` VARCHAR(6) NULL,
-  PRIMARY KEY (`username`),
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC))
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `chat`.`message`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `chat`.`message` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE IF NOT EXISTS `chat`.`message` (
-  `sender` VARCHAR(25) NOT NULL,
-  `receiver` VARCHAR(25) NOT NULL,
-  `MsgID` INT NOT NULL AUTO_INCREMENT,
-  `Message` TEXT(3000) NULL,
-  PRIMARY KEY (`MsgID`),
-  INDEX `fk_table1_users_idx` (`sender` ASC),
-  INDEX `fk_table1_users1_idx` (`receiver` ASC),
-  CONSTRAINT `fk_table1_users`
-    FOREIGN KEY (`sender`)
-    REFERENCES `chat`.`login` (`username`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_table1_users1`
-    FOREIGN KEY (`receiver`)
-    REFERENCES `chat`.`login` (`username`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `chat`.`friend`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `chat`.`friend` ;
-
-CREATE TABLE IF NOT EXISTS `chat`.`friend` (
-  `username` VARCHAR(25) NOT NULL,
-  `friend` VARCHAR(25) NOT NULL,
-  PRIMARY KEY (`username`, `friend`),
-  INDEX `fk_login_has_login_login2_idx` (`friend` ASC),
-  INDEX `fk_login_has_login_login1_idx` (`username` ASC),
-  CONSTRAINT `fk_login_has_login_login1`
-    FOREIGN KEY (`username`)
-    REFERENCES `chat`.`login` (`username`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_login_has_login_login2`
-    FOREIGN KEY (`friend`)
-    REFERENCES `chat`.`login` (`username`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `chat`.`login`
--- -----------------------------------------------------
-START TRANSACTION;
+--
+-- Database: `chat`
+--
+CREATE DATABASE IF NOT EXISTS `chat` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `chat`;
-INSERT INTO `chat`.`login` (`username`, `password`, `admin`, `Email`, `Gender`) VALUES ('saleh', 'saleh', TRUE, 'saleh1204@hotmail.com', 'male');
-INSERT INTO `chat`.`login` (`username`, `password`, `admin`, `Email`, `Gender`) VALUES ('sas', 'sas', FALSE, 'sas@hotmail.com', 'male');
 
-COMMIT;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `friend`
+--
+
+DROP TABLE IF EXISTS `friend`;
+CREATE TABLE `friend` (
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `friend` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Truncate table before insert `friend`
+--
+
+TRUNCATE TABLE `friend`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE `login` (
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `admin` tinyint(1) DEFAULT '0',
+  `Email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Gender` varchar(6) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Truncate table before insert `login`
+--
+
+TRUNCATE TABLE `login`;
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `password`, `admin`, `Email`, `Gender`) VALUES
+('ahmed', '$1$T5b6tBvH$y4ZxrL3fDnm.lNAxQEws30', 1, 'ahmed@ahmed.com', 'female');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+  `sender` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `receiver` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `MsgID` int(11) NOT NULL,
+  `Message` text COLLATE utf8_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Truncate table before insert `message`
+--
+
+TRUNCATE TABLE `message`;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `friend`
+--
+ALTER TABLE `friend`
+  ADD PRIMARY KEY (`username`,`friend`),
+  ADD KEY `fk_login_has_login_login2_idx` (`friend`),
+  ADD KEY `fk_login_has_login_login1_idx` (`username`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `Email_UNIQUE` (`Email`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`MsgID`),
+  ADD KEY `fk_table1_users_idx` (`sender`),
+  ADD KEY `fk_table1_users1_idx` (`receiver`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `MsgID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `friend`
+--
+ALTER TABLE `friend`
+  ADD CONSTRAINT `fk_login_has_login_login1` FOREIGN KEY (`username`) REFERENCES `login` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_login_has_login_login2` FOREIGN KEY (`friend`) REFERENCES `login` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `fk_table1_users` FOREIGN KEY (`sender`) REFERENCES `login` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_table1_users1` FOREIGN KEY (`receiver`) REFERENCES `login` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
